fbe0d7a92c933a69436c86ce438951fcbb577645
Madhurim Gupta <57004824+madhurim010@users.noreply.github.com>
2021-05-22 15:10:03 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,37 @@
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshow");
+  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+}
+function ShowSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slideshow");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+  setTimeout(ShowSlides, 10000); // Change image every 2 seconds
+}
